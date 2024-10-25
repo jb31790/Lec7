9a7082c4bb0b3fbfb8264e2711d51629004bd201
JB <jb@fakemail.com>
2024-10-25 12:23:27 +0200

    task1

---
+++ task_1.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+let num = 42;
+let strNum = String(num);
+
+console.log(`num: ${num}`);
+console.log(`typeof(num): ${typeof num}`);
+
+console.log(`strNum: ${num}`);
+console.log(`typeof(strNum): ${typeof num}`);
